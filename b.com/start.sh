c886646919085e994af3e3b6e650340c0e7dcf2e
monius <m1105930979@gmail.com>
2018-12-03 12:46:36 +0800

    version 2.1

--- b.com/start.sh
+++
@@ -1,8 +0,0 @@
-sudo mkdir /etc/nginx/certs
-sudo /home/monius/Documents/code/flask-deploy/b.com/b.com.sh
-sudo /home/monius/Documents/code/flask-deploy/b.com/b.com.run
-sudo cp /home/monius/Documents/code/flask-deploy/b.com/b.com.conf /etc/nginx/sites-enabled/
-sudo cp /home/monius/Documents/code/flask-deploy/b.com/b.com.conf /etc/nginx/sites-available/
-sudo cp /home/monius/Documents/code/flask-deploy/b.com/b.com.service /etc/systemd/system/
-sudo systemctl enable b.com.service
-sudo systemctl start b.com.service
